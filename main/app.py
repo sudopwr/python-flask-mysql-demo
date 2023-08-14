c175071b29e60afe3b51a2d99a81cc2e77205118
sudopwr <sudopwr@gmail.com>
2023-08-14 17:31:27 +0530

    feat: rest api

--- main/app.py
+++ main/app.py
@@ -1,8 +1,26 @@
 """Flask App main module"""
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/products')
-def index():
-    return { "name": "sudo power" }
+@app.route('/products', methods = ["get"])
+def get_products():
+    return { "message": "get api called" }
+
+@app.route('/products/<id>', methods = ["get"])
+def get_by_id_products(id):
+    return { "message": "get by id api called " + id }
+
+@app.route('/products', methods = ["post"])
+def add_products():
+    request_data = request.get_json()
+    request_data["msg"] = "product added!"
+    return request_data, 201
+
+@app.route('/products', methods = ["put"])
+def update_products():
+    return { "message": "put api called" }
+
+@app.route('/products/<id>', methods = ["delete"])
+def delete_products(id):
+    return { "message": "delete api called " + id }
