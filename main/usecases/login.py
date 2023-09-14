159a89b1b374a25f80e96d0eafe3b1bf7eb7b518
sudopwr <sudopwr@gmail.com>
2023-09-14 17:05:39 +0530

    feat: ep 260 role base auth using decorators

--- main/usecases/login.py
+++ main/usecases/login.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify, g
 import jwt
+from functools import wraps
 
 login_blp = Blueprint("Login", __name__)
 
@@ -13,19 +14,56 @@ def login():
         "uid": username
     }
 
+    if username == "sudopower":
+        payload["role"] = "admin"
+
     token = jwt.encode(payload, "dsfdf5654tgfdg5546", algorithm = "HS256")
 
     return { "token": token }
 
-@login_blp.route('/orders', methods=["get"])
-def get_orders():
-    # verify
-    token = get_token_auth_header()
-    print(token)
-    data = jwt.decode(token, "dsfdf5654tgfdg5546", algorithms = ["HS256"])
-    g.user = data
-    print(g.user)
-    return { "orders" : "success" }
+def login_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token, msg = get_token_auth_header()
+
+        if token == False:
+            return { "error": msg }, 401
+        try:
+            g.user = jwt.decode(token, "dsfdf5654tgfdg5546", algorithms = ["HS256"])
+        except Exception as e:
+            return { "error": "Invalid Token" }, 401
+        
+        return f(*args, **kwargs)
+
+    return decorator
+
+def should_be_admin(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        try:
+            if g.user["role"] != "admin":
+                return { "error": "Invalid request" }, 403
+        except Exception as e:
+            return { "error": "Invalid request" }, 403
+        
+        return f(*args, **kwargs)
+
+    return decorator
+
+@login_blp.route('/products', methods=["get"])
+def get_products():
+    return { "products" : "product data" }
+
+@login_blp.route('/orders', methods=["post"])
+@login_required
+def add_orders():
+    return { "msg" : "order has been placed successfully" }
+
+@login_blp.route('/products', methods=["post"])
+@login_required
+@should_be_admin
+def add_products():
+    return { "msg" : "product has been added successfully" }
 
 
 def get_token_auth_header():
@@ -43,4 +81,4 @@ def get_token_auth_header():
         return False, "Authorization header must be Bearer token"
 
     token = parts[1]
-    return token
+    return token, ''
